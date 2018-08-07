c900806a66bcd67cf6e72226d44942f199114358
Afro <aifric.nolan.gold@gmail.com>
2018-08-07 15:31:14 +0100

    Count words

--- WordCount.py
+++ WordCount.py
@@ -1,3 +1,5 @@
+#Find and counts words in a file
+
 import sys
 import string
 import plotly
@@ -6,8 +8,11 @@ import plotly.graph_objs as go
 def word_count(words, filename):
 	count = {}
 	tokens = []
+	#Adds words into dictionary to be counted
 	for word in words:
 		count[word.strip().lower()] = 0
+
+	#Words from file added to a list
 	with open(filename) as f:
 		for sentence in f:
 			sentence = sentence.strip().split()
@@ -16,16 +21,20 @@ def word_count(words, filename):
 
 	for word in tokens:
 		word = word.strip(string.punctuation)
+
+		#If the word is one of the given words, increment its count by one
 		if word in count:
 			count[word] += 1
 	return count
 
+#Will graph the results in the dictionary
 def graph(count):
 	words = []
 	stats = []
 	for key in count:
 		words.append(key)
 		stats.append(count[key])
+	#Graph in bar chart
 	data = [go.Bar(x=[w for w in words], y = [num for num in stats])]
 	plotly.offline.plot(data, filename="results.html", auto_open=True)
 
